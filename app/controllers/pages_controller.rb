e9a5dc4bfb584b89f4dbc9d7876faeee7142cfaf
Quentin Bonnafet <quentin.bonnafet@gmail.com>
2021-07-16 10:45:04 +0200

    contact form + route + mailer html+ page controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,15 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def contact
+    
+    @contact_first_name = params[:first_name]
+    @contact_last_name = params[:last_name]
+    @sender_email = params[:email]
+    @message = params[:message]
+    ContactFormMailer.with(contact_first_name: @contact_first_name, contact_last_name: @contact_last_name, sender_email: @send_email, message: @message).send_email.deliver_now
+    flash[:notice] = "Message envoyé"
+
+  end
 end
--- app/mailers/contact_form_mailer.rb
+++ app/mailers/contact_form_mailer.rb
@@ -1,2 +1,14 @@
 class ContactFormMailer < ApplicationMailer
+
+  default from: "q_lancien@hotmail.fr"
+
+  def send_email
+
+    @contact_first_name = params[:contact_first_name]
+    @contact_last_name = params[:contact_last_name]
+    @sender_email = params[:sender_email]
+    @message = params[:message]
+
+    mail(to: "quentin.bonnafet@gmail.com", subject: "Vous avez un contact!")
+  end
 end
